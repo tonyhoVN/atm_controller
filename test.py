aaee50b952a72da79030bcd0bb8c98358859d95e
Tony_Ho <55727000+hoanhtung2000@users.noreply.github.com>
2023-11-05 23:11:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,34 +4,33 @@ from ATM_controller import *
 list_cards = [{"card_number":"100", "pin":"1234", "accounts":{"01":3, "02":4}},
               {"card_number":"101", "pin":"1234", "accounts":{"01":5, "02":6}}]
 
-# Declare ATM
-atm = ATMController(list_cards)
-
-# Declare test case 
-test = {
-    "card_number": {"card_number":"102", "pin":"1234"},
-    "pin": {"card_number":"100", "pin":"1235"},
+# Declare test_cases case 
+test_cases = {
+    "card_number": {"card_number":"102", "pin":"1234", "account":"01", "action":[]},
+    "pin": {"card_number":"100", "pin":"1235", "account":"01", "action":[]},
     "account": {"card_number":"100", "pin":"1234", "account":"03", "action":[]}, 
     "balance": {"card_number":"100", "pin":"1234", "account":"01", "action":[("See Balance", None)]},
     "withdraw": {"card_number":"100", "pin":"1234", "account":"02", "action":[("Withdraw", 1),("Withdraw", 5)]},
     "deposit": {"card_number":"100", "pin":"1234", "account":"01", "action":[("Deposit", 5)]}
 }
 
+# Declare ATM and input all cards 
+atm = ATMController(list_cards)
+
 def test_card_number():
-    assert atm.test_case(test["card_number"]) == "wrong card"
+    assert atm.execute(test_cases["card_number"]) == "wrong card"
 
 def test_pin_number():
-    assert atm.test_case(test["pin"]) == "wrong pin"
+    assert atm.execute(test_cases["pin"]) == "wrong pin"
 
 def test_account_number():
-    assert atm.test_case(test["account"]) == "wrong account"
+    assert atm.execute(test_cases["account"]) == "wrong account"
 
 def test_balance():
-    assert atm.test_case(test["balance"]) == [(3,True)]
+    assert atm.execute(test_cases["balance"]) == [(3,True)]
 
 def test_withdraw():
-    assert atm.test_case(test["withdraw"]) == [(3, True),(3, False)]
+    assert atm.execute(test_cases["withdraw"]) == [(3, True),(3, False)]
 
 def test_deposit():
-    assert atm.test_case(test["deposit"]) == [(8, True)]
-
+    assert atm.execute(test_cases["deposit"]) == [(8, True)]
