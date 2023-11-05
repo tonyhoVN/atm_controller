89f029c96af424b9fe7b46ebfe8f0c07d38ab6f5
Tony_Ho <55727000+hoanhtung2000@users.noreply.github.com>
2023-11-05 18:25:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,33 @@
 from ATM_controller import *
+import pytest
 
-
+# Declare list cards 
 list_cards = [{"card_number":"100", "pin":"1234", "accounts":{"00":3, "02":4}},
               {"card_number":"101", "pin":"1234", "accounts":{"00":5, "02":6}}]
 
-test = {"card_number":"101", "pin":"1234", "account":"02", "action":[("See Balance", None), ("Deposit", 100), ("Withdraw", 200)]}
-
+# Declare ATM
 atm = ATMController(list_cards)
-print(atm.test_case(test))
+
+# Declare test case 
+test = {
+    "card_number": {"card_number":"102", "pin":"1234"},
+    "pin": {"card_number":"100", "pin":"1235", "account":"02", "action":[("See Balance", None)]},
+    "balance": {"card_number":"100", "pin":"1234", "account":"02", "action":[("See Balance", None)]},
+    "withdraw": {"card_number":"100", "pin":"1234", "account":"02", "action":[("Withdraw", 1)]},
+}
+
+print(atm.test_case(test["card_number"]))
+
+def test_card_number():
+    assert atm.test_case(test["card_number"]) == "wrong card"
+
+def test_pin_number():
+    assert atm.test_case(test["pin"]) == "wrong pin"
+
+def test_balance():
+    assert atm.test_case(test["balance"]) == [(4,True)]
+
+
+
+
+
